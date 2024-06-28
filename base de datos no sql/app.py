72f4e1175139a64f63eaf88496dacc5061db74a2
EC2 Default User <ec2-user@ip-172-31-70-251.ec2.internal>
2024-06-28 00:52:50 +0000

    creado archivo de imagen de docker

--- base de datos no sql/app.py
+++ base de datos no sql/app.py
@@ -8,12 +8,23 @@ tabla = dynamodb.Table('tabla-andres-zapata')
 #Leer un elemento de la tabla
 response = tabla.get_item(Key={'id': '2'})
 
-print(response['Item'])
+
+#print(response['Item'])
 
 #Leer todos los elementos de la tabla
-response = tabla.scan()
+#response = tabla.scan()
 
-print(response['Items'])
+#print(response['Items'])
+
+response = tabla.update_item(
+    Key={
+        'id': '3'  
+    },
+    UpdateExpression='SET edad = :val1',  # Expresión de actualización
+    ExpressionAttributeValues={
+        ':val1': 34  # Nuevo valor para atributo2
+    }
+)
 
 item = {
     "id": "3",
